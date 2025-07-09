15510594baee80fa0d400f7dad54797f536077e6
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2025-07-10 03:52:22 +0700

    fix: correct import path for NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import NavBar from "./components/navbar";
+import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Education from "./components/Education";
 import Skills from "./components/Skills";
